905e0cfc434b8c034867e89d2b2ee3770222d24e
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2025-04-25 14:09:57 -0300

    feat: implemented user persistence method in the database

--- src/CashFlow.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,41 +1,66 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
+using FluentValidation.Results;
 
 namespace CashFlow.Application.UseCases.User.Register;
 public class RegisterUserUseCase : IRegisterUserUseCase
 {
 
     private readonly IMapper _mapper;
-
     private readonly IPasswordEncripter _passwordEncripter;
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IUserWhiteOnlyRepository _userWhiteOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public RegisterUserUseCase(IMapper mapper, IPasswordEncripter passwordEncripter)
+    public RegisterUserUseCase(
+        IMapper mapper,
+        IPasswordEncripter passwordEncripter,
+        IUserReadOnlyRepository userReadOnlyRepository,
+        IUserWhiteOnlyRepository userWhiteOnlyRepository,
+        IUnitOfWork unitOfWork)
     {
         _mapper = mapper;
         _passwordEncripter = passwordEncripter;
-
+        _userReadOnlyRepository = userReadOnlyRepository;
+        _userWhiteOnlyRepository = userWhiteOnlyRepository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
-        Validate(request);
+        await Validate(request);
 
         var user = _mapper.Map<Domain.Entities.User>(request);
         user.Password = _passwordEncripter.Encrypt(request.Password);
+        user.UserIdentifier = Guid.NewGuid();
+
+        await _userWhiteOnlyRepository.Add(user);
+        await _unitOfWork.Commit();
 
         return new ResponseRegisteredUserJson
         {
             Name = user.Name,
         };
+
     }
 
-    private void Validate(RequestRegisterUserJson request)
+    private async Task Validate(RequestRegisterUserJson request)
     {
         var result = new UserValidator().Validate(request);
+
+        var emailExists = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+        if (emailExists)
+        {
+            result.Errors.Add(new ValidationFailure(string.Empty, ResourceErrorMessages.EMAIL_ALREADY_EXISTS));
+        }
+
 
         if (!result.IsValid)
         {
--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
@@ -25,7 +26,8 @@ public static class DependencyInjectionExtension
         services.AddScoped<IExpensesReadOnlyRepository, ExpensesRepository>();
         services.AddScoped<IExpensesWhiteOnlyRepository, ExpensesRepository>();
         services.AddScoped<IExpensesUpdateOnlyRepository, ExpensesRepository>();
-
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWhiteOnlyRepository, UserRepository>();
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
