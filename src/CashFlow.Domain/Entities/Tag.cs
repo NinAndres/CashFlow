a59f493beb1165d97c48d99655132d9c2b97c603
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2025-05-03 10:51:31 -0300

    feat: added entity Tag and enum Tags

---
+++ src/CashFlow.Domain/Entities/Tag.cs
@@ -0,0 +1,10 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities;
+public class Tag
+{
+    public int Id { get; set; }
+    public Tags Value { get; set; }
+    public long ExpenseId { get; set; }
+    public Expense Expense { get; set; } = default!;
+}
